6c7b4106df009385ac689609091ed374a3a6853b
gust <3065980942@qq.com>
2024-11-14 14:30:04 +0800

    deploy ...

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+    solidity: "0.8.27",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_SEPOLIA_URL,
+            accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`],
+        },
+    },
 };
